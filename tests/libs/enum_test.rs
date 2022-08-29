93086a11bfb3037cba059015400cfa0f37e8269f
yi-ge <a@wyr.me>
2022-08-29 14:16:30 +0800

    test: 测试枚举解构

---
+++ tests/libs/enum_test.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub enum EnumTest {
+    None,
+    NODE(String, Box<String>),
+}
+
+#[test]
+fn new() {
+    println!("{:?}", EnumTest::None);
+    println!(
+        "{:?}",
+        EnumTest::NODE(String::from("abc"), Box::new(String::from("def")))
+    );
+
+    // 解构
+    let bb = Box::new(String::from("def"));
+    let a = EnumTest::NODE(String::from("abc"), bb);
+    let res = match a {
+        EnumTest::None => None,
+        EnumTest::NODE(a, b) => {
+            println!("a: {:?}", a);
+            println!("b: {:?}", b);
+            // println!("bb == b: {:?}", bb == b);
+            Some(b)
+        }
+    };
+
+    assert_eq!(*(res.unwrap()), String::from("def"));
+}
--- tests/libs/mod.rs
+++ tests/libs/mod.rs
@@ -1,2 +1,3 @@
 pub mod list_node_test;
 pub mod list_test;
+pub mod enum_test;
