6533e90559df21b199f26c8147dee40019688b8d
yi-ge <a@wyr.me>
2022-10-02 20:17:01 +0800

    test: 数组中的第K个最大元素

--- tests/array/kth_largest_element_in_an_array_test.rs
+++ tests/array/kth_largest_element_in_an_array_test.rs
@@ -15,4 +15,8 @@ fn find_kth_largest() {
     let nums = vec![3, 2, 3, 1, 2, 4, 5, 5, 6];
     let k = 4;
     assert_eq!(Solution::find_kth_largest(nums, k), 4);
+
+    let nums = vec![1];
+    let k = 1;
+    assert_eq!(Solution::find_kth_largest(nums, k), 1);
 }
