ce48e2b6b445869e04dd15852bc93041f53bdd4e
yi-ge <a@wyr.me>
2023-04-23 10:24:21 +0800

    add: 填充书架

--- src/array/filling_bookcase_shelves.rs
+++ src/array/filling_bookcase_shelves.rs
@@ -6,6 +6,20 @@ pub struct Solution;
 
 impl Solution {
     pub fn min_height_shelves(books: Vec<Vec<i32>>, shelf_width: i32) -> i32 {
-
+        let mut dp = vec![0; books.len() + 1];
+        for i in 1..=books.len() {
+            let mut width = books[i - 1][0];
+            let mut height = books[i - 1][1];
+            dp[i] = dp[i - 1] + height;
+            for j in (1..i).rev() {
+                if width + books[j - 1][0] > shelf_width {
+                    break;
+                }
+                width += books[j - 1][0];
+                height = height.max(books[j - 1][1]);
+                dp[i] = dp[i].min(dp[j - 1] + height);
+            }
+        }
+        dp[books.len()]
     }
 }
--- tests/array/filling_bookcase_shelves_test.rs
+++ tests/array/filling_bookcase_shelves_test.rs
@@ -3,10 +3,22 @@ use rust_practice::array::filling_bookcase_shelves::Solution;
 #[test]
 fn min_height_shelves() {
     // 示例 1：
+    let books = vec![
+        vec![1, 1],
+        vec![2, 3],
+        vec![2, 3],
+        vec![1, 1],
+        vec![1, 1],
+        vec![1, 1],
+        vec![1, 2],
+    ];
+    let shelf_width = 4;
+    assert_eq!(Solution::min_height_shelves(books, shelf_width), 6);
 
     // 示例 2:
     // 输入: books = [[1,3],[2,4],[3,2]], shelfWidth = 6
     // 输出: 4
-
-    assert_eq!(Solution::min_height_shelves(), 1);
+    let books = vec![vec![1, 3], vec![2, 4], vec![3, 2]];
+    let shelf_width = 6;
+    assert_eq!(Solution::min_height_shelves(books, shelf_width), 4);
 }
